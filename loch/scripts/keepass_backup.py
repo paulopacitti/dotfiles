dd739ca8098338cdc445a6c4372ca3a6965e281c
paulopacitti <ppacitti@outlook.com>
2023-07-24 14:59:24 -0300

    Updated keepass rclone backup script

--- loch/scripts/keepass_backup.py
+++ loch/scripts/keepass_backup.py
@@ -1,9 +1,11 @@
-# Crontab:
-# 0 10 * * 1,4 python /Users/paulopacitti/documents/scripts/keepass_backup.py
+# Crontab: 0 10 * * 1,4 ${which python} /Users/paulopacitti/documents/scripts/keepass_backup.py
 
 import os
+import shutil
 from datetime import datetime
 
+# rclone path
+rclone_exec = "/opt/homebrew/bin/rclone"
 # scripts path
 scripts_path = os.path.expanduser("~/documents/scripts") 
 # Path to KeePass database
@@ -16,7 +18,8 @@ backuped_db_filename = f"{now}-{keepass_db_filename}"
 
 # rclone copy to Google Drive
 os.system(f"cp {keepass_db_path} {scripts_path}/{backuped_db_filename}")
-print(f"Backuped {backuped_db_filename} to Google Drive!")
+os.system(f"{rclone_exec} copy {scripts_path}/{backuped_db_filename} keepass:")
+print(f"Backed up {backuped_db_filename} to Google Drive!")
 
 # delete uploaded file
 os.system(f"rm -rf {backuped_db_filename}")
