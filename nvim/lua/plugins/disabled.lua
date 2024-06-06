47178c825b5022fe513f45dfed1ab9ecedb0dd16
paulopacitti <ppacitti@outlook.com>
2024-06-06 09:57:43 -0300

    Add nvim config

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  -- disable trouble
+  { "rcarriga/nvim-notify", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+}
---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    },
+  },
+}
